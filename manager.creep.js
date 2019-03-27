15d383c93cb391f7d26adf257a5d9734618cac64
Thomas Nowak <tnowak@plex.com>
2019-03-27 16:11:48 -0400

    tweak exception handling further

--- manager.creep.js
+++ manager.creep.js
@@ -12,31 +12,30 @@ function run(creep) {
   let taskName = creep.memory.task;
   let options = creep.memory.options;
   if(say) creep.say(`${taskName}`);
-
-  let role = roleFactory.create(creep.memory.role);
-  
-  if(taskName) {
-    if(role.tasks.find(t => t.name === taskName).end(creep)) {
-      creep.memory.task = undefined;
-    }
-    else {
-      try {
+  try {
+    let role = roleFactory.create(creep.memory.role);
+    
+    if(taskName) {
+      if(role.tasks.find(t => t.name === taskName).end(creep)) {
+        creep.memory.task = undefined;
+      }
+      else {
         let execution = executionFactory.create(taskName);
         execution.run(creep, options);
       }
-      catch (ex) {
-        console.log(creep.name, ex);
+    }
+    else {
+      let task = role.tasks.find(t => t.ready(creep));
+      if(task) {
+        creep.memory.task = task.name;
+        creep.memory.options = task.options;
+      }
+      else {
+        creep.say("Idle");
       }
     }
   }
-  else {
-    let task = role.tasks.find(t => t.ready(creep));
-    if(task) {
-      creep.memory.task = task.name;
-      creep.memory.options = task.options;
-    }
-    else {
-      creep.say("Idle");
-    }
+  catch (ex) {
+    console.log(`${creep.name} error while running ${taskName}:`, ex);
   }
 }
