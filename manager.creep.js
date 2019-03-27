dcf7f713beb27c2a0b23e3f6791323d124b00fac
Thomas Nowak <tnowak@plex.com>
2019-03-27 17:03:01 -0400

    debugging

--- manager.creep.js
+++ manager.creep.js
@@ -4,6 +4,8 @@ let logger = require('constant.logger');
 
 const say = false;
 
+const debugName = '';
+
 module.exports = {
   run: run
 }
@@ -12,21 +14,34 @@ function run(creep) {
   let taskName = creep.memory.task;
   let options = creep.memory.options;
   if(say) creep.say(`${taskName}`);
+
+
   try {
+    if(debugName == creep.name)
+      console.log(`Debugging ${creep.name}`)
+
     let role = roleFactory.create(creep.memory.role);
     
     if(taskName) {
+      if(debugName == creep.name)
+        console.log(`  task:${taskName}`)
       if(role.tasks.find(t => t.name === taskName).end(creep)) {
         creep.memory.task = undefined;
       }
       else {
+        if(debugName == creep.name)
+          console.log(`  executing...`)
         let execution = executionFactory.create(taskName);
         execution.run(creep, options);
       }
     }
     else {
+      if(debugName == creep.name)
+        console.log(`  finding new task`)
       let task = role.tasks.find(t => t.ready(creep));
       if(task) {
+        if(debugName == creep.name)
+          console.log(`task found: ${task.name}`)
         creep.memory.task = task.name;
         creep.memory.options = task.options;
       }
