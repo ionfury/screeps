cfa8488d9b4e4bbc141e3933f6eedd63e98a18e3
ionfury <ionfury@gmail.com>
2019-03-26 23:46:33 -0400

    Revert "add inheritence and class to roles"

---
+++ creep.role.undefined.js
@@ -0,0 +1,15 @@
+let logger = require('constant.logger');
+
+const name = 'undefined';
+
+module.exports = {
+  name: name,
+  run: run,
+  body: [],
+  options: options,
+  spawn: spawn
+};
+
+function run(self){}
+function options(options){}
+function spawn(options){}
