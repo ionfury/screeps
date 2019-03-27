48071a0c4238cfa71b81f553ebc5485ca9f3513e
Thomas Nowak <tnowak@plex.com>
2019-03-27 17:40:50 -0400

    save

--- creep.task.goToRoom.js
+++ creep.task.goToRoom.js
@@ -5,6 +5,8 @@ module.exports = {
 function goToRoom(self, options) {
   let route = Game.map.findRoute(self.room.name, self.memory[options.destination]);
 
+  console.log(route.length)
+
   if(route.length > 0) {
     let exit = self.pos.findClosestByRange(route[0].exit);
     self.moveTo(exit);  
