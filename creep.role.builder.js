2c67a0984234d85379178f904370fb09067b4f79
tom <ionfury@gmail.com>
2019-03-28 02:35:24 -0400

    go back t o 2 builders and upgraders

--- creep.role.builder.js
+++ creep.role.builder.js
@@ -58,5 +58,5 @@ function spawn(options){
     .map(c => c.memory.role)
     .reduce((acc, role) => (acc[role] = (acc[role] || 0) + 1, acc), {});
   let count = creepsInRoom[name] || 0;
-  return count < 3;
+  return count < 2;
 }
